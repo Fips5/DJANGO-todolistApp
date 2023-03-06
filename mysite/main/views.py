29ebb7885e93c7fbbc09bf92f74aaeca1986c94e
Fips <david00badea@gmail.com>
2023-03-06 22:23:12 +0200

    this is the code

---
+++ mysite/main/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import ToDoList
+from .models import Item 
+from .forms import CreateNewList
+# Create your views here.
+
+
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+ 
+    if response.method == "POST":
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+                item.save()
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+            if len(txt) > 2:
+                ls.item_set.create(text=txt, complete=False)
+            else:
+             print("invalid")
+    return render(response, "main/list.html", {"ls":ls})
+
+def home(response):
+	return render(response, "main/home.html", {})
+
+def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            response.user.todolist_set.create(name=n)
+
+            return HttpResponseRedirect("/%i" % response.user.todolist_set.last().id)
+
+    else:
+        form = CreateNewList()
+
+    return render(response, "main/create.html", {"form":form})
